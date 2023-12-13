8d96fba4a6474a83433fc26f031cc218bb352a6d
liam-powers <95184359+liam-powers@users.noreply.github.com>
2023-12-13 01:27:44 -0500

    unignored .next dir

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/liam/Documents/Code/Serious Projects/pw_next/project/src/app/layout":["static/media/86fdec36ddd9097e-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
