7325c8c521686787f112809345e6e9b98028f4d0
liam-powers <95184359+liam-powers@users.noreply.github.com>
2024-03-15 21:17:27 -0500

    converted jpgs to webps for vastly improved loading times

--- src/app/components/FilmShowcaseGrid.tsx
+++ src/app/components/FilmShowcaseGrid.tsx
@@ -4,9 +4,9 @@ import ImageGallery from "react-image-gallery";
 import "../../../node_modules/react-image-gallery/styles/css/image-gallery.css";
 
 const images = Array.from({ length: 20 }, (_, index) => ({
-    original: `/filmphotos/${index + 1}.jpg`,
+    original: `/filmphotos/${index + 1}.webp`,
     alt: `Image ${index + 1}`,
-    thumbnail: `/filmphotos/${index + 1}.jpg`,
+    thumbnail: `/filmphotos/${index + 1}.webp`,
 }));
 
 export default function FilmShowcaseGrid() {
